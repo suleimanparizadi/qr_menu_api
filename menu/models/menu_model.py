8dd9c0ef70c1cb4d3c87ebb9a8dd27bd7714dde6
suleimanparizadi <suleiman.parizadi.30@gmail.com>
2026-08-19 10:57:45 +0330

    adding some more feture to models

--- menu/models/menu_model.py
+++ menu/models/menu_model.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 import qrcode
-from django.core.files import File
 from io import BytesIO
 from django.conf import settings
+from django.core.files.base import ContentFile
+
 
 
 User = get_user_model()
@@ -16,9 +17,12 @@ class QRMenu(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='menus')
     title = models.CharField(max_length=225)
     description = models.CharField(max_length=350, blank=True, null=True)
-    qr_code = models.ImageField(upload_to='qr_menu/')
+    qr_code = models.ImageField(upload_to='qr_menu/', null=True, blank=True) # to create menu first then generate the qr image
     available = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
 
 
     def generate_qr_code(self):
@@ -32,8 +36,9 @@ class QRMenu(models.Model):
         if self.qr_code and self.qr_code.storage.exists(self.qr_code.name):
             self.qr_code.delete(save=False)
 
-        self.qr_code.save(filename, ContentFile=(qr_io.getvalue()), save=True)
+        self.qr_code.save(filename, ContentFile(qr_io.getvalue()), save=True)
         qr_io.close()
+
 
 
     def __str__(self):
@@ -41,13 +46,51 @@ class QRMenu(models.Model):
 
 
 
-class MenuItem(models.Model):
 
-    menu = models.ForeignKey(QRMenu, on_delete=models.CASCADE, related_name='items')
-    item = models.CharField(max_length=225)
-    description = models.CharField(max_length=225)
-    price = models.IntegerField()
-    available = models.BooleanField(default=True)
+
+class MenuSection(models.Model):
+
+    menu = models.ForeignKey(QRMenu, on_delete=models.CASCADE, related_name='sections')
+    name = models.CharField(max_length=225)
+    start_time = models.TimeField(null=True, blank=True)    
+    end_time = models.TimeField(null=True, blank=True)
+    order = models.PositiveIntegerField(default=0) # in which order sections will be shows
+
+
+    class Meta:
+        ordering = ['order']
+
 
     def __str__(self):
-        return f"{self.item} - {self.menu} - {self.id}"
+        return f"{self.name} - {self.menu.title}"
+
+
+
+class MenuItem(models.Model):
+
+    section = models.ForeignKey(MenuSection, on_delete=models.CASCADE, related_name='items')
+    item = models.CharField(max_length=225)
+    description = models.CharField(max_length=225, blank=True, null=True)
+    price = models.IntegerField()
+    order = models.PositiveIntegerField(default=0) # to show which item first
+    available = models.BooleanField(default=True)
+
+
+    class Meta:
+        ordering = ['order']
+
+
+    def __str__(self):
+        return f"{self.item} - {self.section.name} - {self.id}"
+
+
+
+class MenuView(models.Model):
+    menu = models.ForeignKey(QRMenu, on_delete=models.CASCADE, related_name='views')
+    viewed_at = models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+        ordering = ['-viewed_at']
+
+
